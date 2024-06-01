b2494b6f08c2e5f5f594df300c95b5cfe239647f
rhonnyjaimes <rhonnyjaimes2002@gmail.com>
2024-06-01 19:50:52 -0400

    rutas

---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
+
+router.post('/', usuarioController.agregarUsuario.bind(usuarioController));
+router.put('/:id', usuarioController.editarUsuario.bind(usuarioController));
+router.delete('/:id', usuarioController.eliminarUsuario.bind(usuarioController));
+router.get('/', usuarioController.listarUsuarios.bind(usuarioController));
+router.get('/:id', usuarioController.obtenerDetallesUsuario.bind(usuarioController));
+
+module.exports = router;
